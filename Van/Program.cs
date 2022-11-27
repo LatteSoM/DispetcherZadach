c9d8b72844ed0dd9816abc3f5e449aba913d807d
LatteSoM <staz1n228@gmail.com>
2022-11-28 01:49:53 +0300

    D:/Git/

---
+++ Van/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+using System.Linq;
+
+namespace Van
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            ProcessUs.Zpusk();
+        } 
+    }
+}
